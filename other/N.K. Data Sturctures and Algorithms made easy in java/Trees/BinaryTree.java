0f9ee7e29c77ef15b5e3b3186041f1b3af317a9f
Snigdha <u5612813@uds.anu.edu.au>
2018-01-06 19:26:44 +0530

    BinaryTree DataStructure

---
+++ other/N.K. Data Sturctures and Algorithms made easy in java/Trees/BinaryTree.java
@@ -0,0 +1,16 @@
+public class BinaryTree
+{
+	static class Node
+	{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int d)
+		{
+			this.data = d;
+			left = null;
+			right = null;
+		}
+	}
+}
