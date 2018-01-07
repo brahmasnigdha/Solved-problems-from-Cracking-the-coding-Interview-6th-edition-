879d4408b5801f434dc32e9eb17d7e7c177cfaa2
Snigdha <u5612813@uds.anu.edu.au>
2018-01-07 11:35:35 +0530

    Binary tree data structure

--- other/N.K. Data Sturctures and Algorithms made easy in java/Trees/BinaryTree.java
+++ other/N.K. Data Sturctures and Algorithms made easy in java/Trees/BinaryTree.java
@@ -13,4 +13,84 @@ public class BinaryTree
 			right = null;
 		}
 	}
+	
+	Node root;
+
+	Node addLeftChild(int data, Node node)
+	{
+		Node newChild = new Node(data);
+		node.left = newChild;
+		return node.left;	
+	}
+	
+	Node addRightChild(int data, Node node)
+	{
+		Node newChild = new Node(data);
+		node.right = newChild;
+		return node.right;
+	}
+	//preorder traversal
+	void preorderTraversal(Node node)
+	{
+		if(node != null)
+		{
+			System.out.print(node.data+" ");
+			preorderTraversal(node.left);
+			preorderTraversal(node.right);
+		}
+	}	
+	
+	//inorder traversal
+	void inorderTraversal(Node node)
+	{
+		if(node != null)
+		{
+			inorderTraversal(node.left);
+			System.out.print(node.data+" ");
+			inorderTraversal(node.right);
+		}
+	}
+
+	//postorder traversal
+	void postorderTraversal(Node node)
+	{
+		if(node != null)
+		{
+			postorderTraversal(node.left);
+			postorderTraversal(node.right);
+			System.out.print(node.data+" ");
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		BinaryTree tree = new BinaryTree();
+		
+		tree.root = new Node(1);	
+		
+		Node root_left = tree.addLeftChild(2, tree.root);
+		
+		Node root_right = tree.addRightChild(3, tree.root);
+		
+		Node root_left_left = tree.addLeftChild(4, root_left);
+		
+		Node root_left_right = tree.addRightChild(5, root_left); 
+
+		Node root_right_left = tree.addLeftChild(6, root_right);
+		
+		Node root_right_right = tree.addRightChild(7, root_right);
+
+		
+		System.out.print("Preorder Traversal: ");
+		tree.preorderTraversal(tree.root);
+		System.out.println();
+		
+		System.out.print("Inorder Traversal: ");
+		tree.inorderTraversal(tree.root);
+		
+		System.out.print("\nPostorder Traversal: ");
+		tree.postorderTraversal(tree.root);
+	}
+	
+	
 }
